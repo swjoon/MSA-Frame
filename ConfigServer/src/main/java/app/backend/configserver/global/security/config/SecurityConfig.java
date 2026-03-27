3eed30404a030588a559c011623ce0af88d1a143
swjoon <jetkid1446@gmail.com>
2026-03-28 06:39:07 +0900

    feat: ConfigServer initial Setting

---
+++ ConfigServer/src/main/java/app/backend/configserver/global/security/config/SecurityConfig.java
@@ -0,0 +1,50 @@
+package app.backend.configserver.global.security.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+	@Bean
+	public BCryptPasswordEncoder bCryptPasswordEncoder() {
+
+		return new BCryptPasswordEncoder();
+	}
+
+	@Bean
+	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
+		http
+			.csrf(AbstractHttpConfigurer::disable)
+
+			.authorizeHttpRequests((auth) -> auth.anyRequest().authenticated())
+
+			.httpBasic(Customizer.withDefaults());
+
+		return http.build();
+	}
+
+	@Bean
+	public UserDetailsService userDetailsService() {
+
+		UserDetails user = User.builder()
+			.username("admin")
+			.password(bCryptPasswordEncoder().encode("1234"))
+			.roles("ADMIN")
+			.build();
+
+		return new InMemoryUserDetailsManager(user);
+	}
+}
